ac9c626e12170e7477b53be817bbded717273711
Gabriel Hernandez <gabrieldejesushernandez@gmail.com>
2022-01-30 21:38:17 -0600

    Array pactices done

--- CodeAlongsJS/sumOfRange..js
+++ CodeAlongsJS/sumOfRange..js
@@ -1,7 +1,12 @@
 var testArray = [17, 42, 311, 5, 9, 10, 28, 7, 6];
-
 var sum = 0;
+let newElement = prompt("Enter a number of your choice: ");
+
+
+testArray.push(parseInt(newElement));
 
 for (var position = 0; position < testArray.length; position++) {
     sum += testArray[position];
 }
+
+console.log("The sum of " + testArray + " is: " + sum);
